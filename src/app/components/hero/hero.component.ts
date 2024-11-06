0060f400e1799f24f2a277dbffc25b0a44011108
Safwat Ahmed <safwatahmed669@gmail.com>
2024-11-06 15:59:46 +0200

    Edit about and topbar components

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  emailURL: string =
+    "mailto:safwatahmed669@gmail.com?subject=General Inquiry about [Your Question]&body= Hello, Safwat Ahmed%0D%0A%0D%0AI'm [Your Name] from [Your Company].%0D%0A%0D%0A I'm reaching you for ...";
+  phoneURL: string = 'tel:+201125285475';
+  downloadResume(): void {
+    let resumeURL = 'assets/Safwat Ahmed Resume.pdf';
+    let resumeLink = document.createElement('a');
+    resumeLink.href = resumeURL;
+    resumeLink.download = "Safwat Ahmed Resume.pdf";
+    resumeLink.title = resumeURL;
+    resumeLink.target = "_blank";
+    resumeLink.click();
 
+  }
 }
